b3a4b0859949cca79327df5da7d80fbb65def088
Johannes de Vries <Vrie1542@student.nhl.nl>
2018-06-21 13:07:53 +0200

    Revert "Opdracht C Layout"
    This reverts commit bb3d3be95947a0cf7c3d612b6edd846a82824200.

--- C/TrainTree/TrainTree/Resources/Resource.Designer.cs
+++ C/TrainTree/TrainTree/Resources/Resource.Designer.cs
@@ -41,6 +41,49 @@ namespace TrainTree
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f040005
+			public const int buttonConnect = 2130968581;
+			
+			// aapt resource value: 0x7f040009
+			public const int buttonToggle = 2130968585;
+			
+			// aapt resource value: 0x7f040002
+			public const int editTextIPAddress = 2130968578;
+			
+			// aapt resource value: 0x7f040004
+			public const int editTextIPPort = 2130968580;
+			
+			// aapt resource value: 0x7f040000
+			public const int linearLayout1 = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int textViewIp = 2130968577;
+			
+			// aapt resource value: 0x7f040003
+			public const int textViewPoort = 2130968579;
+			
+			// aapt resource value: 0x7f040007
+			public const int textViewSensor = 2130968583;
+			
+			// aapt resource value: 0x7f040006
+			public const int textViewServerConnect = 2130968582;
+			
+			// aapt resource value: 0x7f040008
+			public const int textViewServo = 2130968584;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
@@ -62,6 +105,21 @@ namespace TrainTree
 			
 			// aapt resource value: 0x7f030000
 			public const int app_name = 2130903040;
+			
+			// aapt resource value: 0x7f030002
+			public const int editTextIp = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int editTextPoort = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int textViewSensor = 2130903044;
+			
+			// aapt resource value: 0x7f030001
+			public const int textViewServerConnect = 2130903041;
+			
+			// aapt resource value: 0x7f030005
+			public const int textViewServo = 2130903045;
 			
 			static String()
 			{
